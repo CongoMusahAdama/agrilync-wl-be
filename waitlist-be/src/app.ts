9915bcac7e8e8e1b5bb6bcaf9709bf69e4eab077
congo musah adama <musahcongoadama@gmail.com>
2025-04-22 21:07:48 +0000

    remove validation

--- waitlist-be/src/app.ts
+++ waitlist-be/src/app.ts
@@ -9,7 +9,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-app.use(cors());
+const allowedOrigins = ['https://agrilync-wl-be.onrender.com', 'https://agrilync.netlify.app'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin like mobile apps or curl requests
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 
 //routes
